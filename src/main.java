209b8adb4e729f9cc4d6f7aee7943844f8fd858b
Jag <jagdhillon077@gmail.com>
2021-02-15 15:34:55 -0800

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        String hi = "String Reverse";
+        String hi2 = new StringBuilder(hi).reverse().toString();
+        System.out.println(hi2);
+    }
+}
